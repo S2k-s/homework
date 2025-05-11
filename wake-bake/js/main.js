925aca3439464688bb4fb5039eb1a6537482c21a
S2k-s <supknife8@gmail.com>
2025-05-12 02:36:47 +0600

    Add files via upload

---
+++ wake-bake/js/main.js
@@ -0,0 +1,122 @@
+(function () {
+
+    // бургер
+
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+
+        if (document.documentElement.clientWidth > 900) return
+
+        // if (burgerIcon) {
+        //     e.preventDefault()
+        // }
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+
+    }
+
+    // модалка
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
+    // табы
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toogleTab)
+
+    function toogleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        const tabContentId = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentId)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--active')
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+
+
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
+
+
+    }
+
+    // Аккордеон
+
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionControl = e.target.closest('.accordion-list__control')
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement
+            const accordionContent = accordionControl.nextElementSibling
+
+            accordionLists.forEach(list => {
+                list.querySelectorAll('.accordion-list__item').forEach(item => {
+                    if (item !== accordionItem) {
+                        item.classList.remove('accordion-list__item--opened')
+                        const contentClosed = item.querySelector('.accordion-list__content')
+                        if (contentClosed) contentClosed.style.maxHeight = null
+                    }
+                });
+            });
+            
+            accordionItem.classList.toggle('accordion-list__item--opened')
+            
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+            } else {
+                accordionContent.style.maxHeight = null
+            }
+            
+        })
+        
+    })
+
+})()
+
+
