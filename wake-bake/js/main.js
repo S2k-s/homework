90ede4bbc452e93b01c9e92f159ec07dca85cb7e
S2k-s <supknife8@gmail.com>
2025-05-15 06:56:11 +0600

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -178,6 +178,12 @@
         },
     });
 
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 99-99-99')
+
+    im.mask(telInputs)
 
 })()
 
