f8f84bcb53a0e581ccfdce7ce5ad3fc5e4425e3c
S2k-s <supknife8@gmail.com>
2025-08-24 01:07:43 +0600

    Add files via upload

---
+++ js/main-10.js
@@ -0,0 +1,35 @@
+// Задача 1.
+// Создайте переменные с корректными именами, чтобы сохранить следующую информацию:
+// 1. firstName – имя (строка)
+// 2. lastName – фамилия (строка)
+// 3. isStudent – является ли учеником курса (булево значение)
+
+const FIRST_NAME = 'Nikolay';
+const SECOND_NAME = 'Aleksandrov';
+const IS_STUDENT = true;
+
+// Задача 2.
+// 1. Объявите переменную age с числовым значением возраста студента.
+// 2. Объявите переменную currentYear и присвойте ей значение текущего года (например, 2025).
+// 3. Используя age и currentYear вычислите год рождения студента, запишите результат в переменную birthYear.
+
+let age = 20;
+let currentYear = 2025;
+const BIRTH_YEAR = currentYear - age;
+
+// Задача 3.
+// Выведите в консоли сообщение в таком формате:
+// Меня зовут [firstName] [lastName], мне [age] лет. Я ученик/ученица курса: [isStudent].
+
+console.log(`Меня зовут ${FIRST_NAME} ${SECOND_NAME}, мне ${age} лет. Я ученик курса: ${IS_STUDENT}`)
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+// 123456789falsetrue
